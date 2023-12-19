f44491391dc8e021b90a8088dcd35b54c96a9cf0
madao1202 <xiebinggo@qq.com>
2023-12-19 20:41:12 +0800

    Update VfxPush.py

--- Code/VfxPush.py
+++ Code/VfxPush.py
@@ -11,7 +11,7 @@ from dingtalkchatbot.chatbot import DingtalkChatbot
 # WebHook地址
 webhook = 'https://oapi.dingtalk.com/robot/send?access_token=a84df00356f9fb088dc8772afafd39638e00f071639f7b0f6dcd1a640948b754'
 secret = 'SECf234ad80a95a7ef035c67384b4e1379080e9b193f7849f5548e8975c90be65fa'
-# 初始化机器人小丁
+# 初始化机器人
 xiaoding = DingtalkChatbot(webhook, secret)  # 方式一：通常初始化方式
 
 
